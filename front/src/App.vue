dd232fc852fb8a9bb23ec8a33e51c887640f22db
Matheus Costa <matt4git@gmail.com>
2025-09-03 19:25:54 -0300

    Adding basic frontend

---
+++ front/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import WelcomeFlow from './components/WelcomeFlow.vue'
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <WelcomeFlow />
+  </div>
+</template>
+
+<style>
+@import './assets/main.css';
+</style>
