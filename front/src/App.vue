49d1b11f6848aa9d2cbf02dacd6bc08988799f77
Matheus Costa <matt4git@gmail.com>
2025-09-15 20:50:52 -0300

    Deleted frontend. Will make something even simpler

--- front/src/App.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <main class="bg-gray-50 dark:bg-gray-900 min-h-screen">
-    <MealManager />
-  </main>
-</template>
-
-<script setup>
-import MealManager from './components/MealManager.vue';
-</script>
